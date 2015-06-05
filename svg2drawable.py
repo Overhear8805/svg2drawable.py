46b4442f3d5a41db5e5b1283b7b7f9a15251a63c
Simon Cedergren <simon@tuxflux.se>
2015-06-05 15:07:20 +0200

    Implemented option to choose between --export-area-page and --export-area-drawing as suggested in issue #2

--- svg2drawable.py
+++ svg2drawable.py
@@ -40,6 +40,7 @@ parser.add_argument("input_files", nargs="+", help="Name of the input_file(s) fi
 parser.add_argument("output_files", nargs="*", help="Name of the output_file file\(.png\)")
 parser.add_argument("--width", type=int, help="Desired width of the drawable \(mdpi\)")
 parser.add_argument("--height", type=int, help="Desired height of the drawable \(mdpi\)")
+parser.add_argument("--no_padding", action="store_true", help="Render only the actual drawing, remove whitespace")
 args = parser.parse_args()
 
 # Windows don't automatically expand wild cards so let's make sure it's expanded in case we're on Windows
@@ -61,6 +62,14 @@ if not args.height and not args.width:
 if len(args.input_files) != len(args.output_files):
     print("ERROR: Invalid argument: specify no output_files or the same amount as input files")
     exit()
+
+'''
+Check if whitespace around the actual drawing should be kept or not.
+'''
+if (args.no_padding):
+    padding_args = "--export-area-drawing"
+else:
+    padding_args = "--export-area-page"
 
 '''
  Create the default folders that is used in Android 
@@ -98,18 +107,18 @@ def render_images(input_file, output_file, width, height):
         # If height is given; use height as the given argument to Inkscape.
         if height:
                 y = int(height)*res_array[index]
-                cmd = "inkscape {0}  --export-area-drawing -e res/drawable-{1} -h{2}".format(input_file, dir_array[index] +"/"+output_file, y)
+                cmd = "inkscape {0}  {1}  -e res/drawable-{2} -h{3}".format(input_file, padding_args, dir_array[index] +"/"+output_file, y)
         
         # If width is given; use width as the given argument to Inkscape.
         elif width:
                 x = int(width)*res_array[index]
-                cmd = "inkscape {0}  --export-area-drawing -e res/drawable-{1} -w{2}".format(input_file, dir_array[index] +"/"+output_file, x) 
+                cmd = "inkscape {0}  {1} -e res/drawable-{2} -w{3}".format(input_file, padding_args, dir_array[index] +"/"+output_file, x) 
         
         # If both width and height is given; use both.
         else:
                 y = int(height)*res_array[index]
                 x = int(width)*res_array[index]
-                cmd = "inkscape {0}  --export-area-drawing -e res/drawable-{1} -w{2} -h{3}".format(input_file, dir_array[index] +"/"+output_file, x, y) 
+                cmd = "inkscape {0}  {1} -e res/drawable-{2} -w{3} -h{4}".format(input_file, padding_args, dir_array[index] +"/"+output_file, x, y) 
        
         # Do the actual rendering
         subprocess.call(cmd.split(), shell=False) 
@@ -118,7 +127,7 @@ def render_images(input_file, output_file, width, height):
 # Run the code above.     
 make_folders()
 for (i, input_file) in enumerate(args.input_files):
-    print input_file
+    print (input_file)
     output_file = get_output_file(args.output_files[i], input_file);
     render_images(input_file, output_file, args.width, args.height)
     print("\n")
